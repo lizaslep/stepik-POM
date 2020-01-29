724a379cbab28a6663d145b7a43affd3ea2ed93f
elizavetas <elizavetas@iMac-Elizavvetas.local>
2020-01-29 15:18:16 +0200

    Task 4.3 part 2

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 import time
 import pytest
 
@@ -47,3 +48,14 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser,link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_see_products_basket()
+    basket_page.should_see_empty_basket_text()
+
+    
