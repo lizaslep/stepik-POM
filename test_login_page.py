0234e3da6c380162b3fcf4f9006d609b6fd67c47
elizavetas <elizavetas@iMac-Elizavvetas.local>
2020-01-30 17:10:35 +0200

    remove test_login_page.py

--- test_login_page.py
+++
@@ -1,7 +0,0 @@
-from pages.login_page import LoginPage
-
-def test_guest_should_get_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
